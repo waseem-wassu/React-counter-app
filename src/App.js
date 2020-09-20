1f387d9427af8e8f02b946e4a2372dd68426c38f
waseem-wassu <waseemwassu8873@gmail.com>
2020-09-20 06:00:37 +0530

    Adding and starting project

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, {useState} from "react";
+import Counter from './components/counter';
+import './App.css';
+
+function App() {
+  let [count,setCount] = useState(0);
+  let [day,setDay] = useState(false);
+
+  return (
+    <div className={` box ${day ? 'dayLight' : 'dayDark'} `}>
+
+    <h1>Now its time to say good {day ? 'morning':'night'}</h1>
+
+       <Counter counter={count} />
+       &nbsp; &nbsp;
+       <button onClick={() => {
+         setCount(++count);
+       }}>Update Count</button>
+         &nbsp; &nbsp;
+       <button onClick = {() => {
+          setDay(!day)
+       }}>Update Day</button>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/counter.js
@@ -0,0 +1,16 @@
+import React, {useState} from "react";
+
+
+function Counter(props) {
+
+    return (
+      <div>
+
+          <h3>The count is : {props.counter}</h3>
+
+
+      </div>
+  );
+}
+
+export default Counter;
