943604e78d1469f79cc48d1b7c44586f11d72434
waseem-wassu <waseemwassu8873@gmail.com>
2020-09-22 13:51:42 +0530

    now running our project on production for deployment

---
+++ build/precache-manifest.790ed5d47729e687e51cffca64302986.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a8a7ff31cbde3602ac274076fe230b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05fd47eea26a4c2221b7",
+    "url": "/static/css/main.1b5b2b42.chunk.css"
+  },
+  {
+    "revision": "592767431073c9afc2e8",
+    "url": "/static/js/2.8518ec05.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8518ec05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05fd47eea26a4c2221b7",
+    "url": "/static/js/main.1bdbdc5d.chunk.js"
+  },
+  {
+    "revision": "04ba97c271dc4ff555e9",
+    "url": "/static/js/runtime-main.e071777d.js"
+  }
+]);
